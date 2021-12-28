b5b20c4593efb26446597e93c5724761e6822ed9
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2021-12-28 09:08:55 -0300

    Commit inicial

---
+++ server.ts
@@ -0,0 +1,14 @@
+import express, { Request, Response } from 'express';
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/people', (request: Request, response: Response) => {
+    response.send("Uau!!!")
+})
+
+app.get('/people/:id', (request: Request, response: Response) => {
+    const id = request.params.id;
+    response.send("Uau!!! " + id)
+})
+
+app.listen(port, () => console.log(`Servidor funcionando na porta ${port}! Uhu!!!!!`));
