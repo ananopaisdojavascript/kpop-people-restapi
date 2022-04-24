4b4ca9fb7e9decc80f3792fff55a2ac062feca0c
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2022-04-24 16:34:03 -0300

    save

--- server.ts
+++ server.ts
@@ -1,9 +1,25 @@
 import express, { Request, Response } from 'express';
+import mysql from 'mysql';
 const app = express();
 const port = process.env.PORT || 5000;
 
+const connection = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB
+});
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/people', (request: Request, response: Response) => {
-    response.send("Hey!!!")
+    const query = 'SELECT * FROM people';
+    connection.query(query, (error, results, fields) => {
+        if (error) throw error;
+        response.send(JSON.stringify(results));
+    })
 })
 
 app.get('/people/:id', (request: Request, response: Response) => {
